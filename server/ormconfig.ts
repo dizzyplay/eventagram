05efe3c92067e95c96c46e8df29994fbad6023d7
vovo <vovoboss@gmail.com>
2019-09-04 15:14:39 +0900

    change db

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -2,7 +2,7 @@ import { ConnectionOptions } from "typeorm";
 
 const connectionOptions:ConnectionOptions = {
   type: "postgres",
-  database: "test_db",
+  database: "postgres",
   synchronize: true,
   port: 5432,
   host: "db",
